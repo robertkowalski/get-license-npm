bc95a50d02b9807ce069ef8ff8454610f5af98a8
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2016-10-10 11:13:05 +0300

    adjust refactoring

---
+++ test/03-error-handling.js
@@ -0,0 +1,24 @@
+const tap = require('tap')
+const test = tap.test
+
+const getLicenseInfo = require('../lib/index.js')
+
+test('invokes callback on module not found error', (t) => {
+  const f = '/fooo/bar/baz'
+
+  getLicenseInfo(f, (err, res) => {
+    tap.ok(/Module not found/.test(err.message));
+
+    t.end()
+  })
+})
+
+test('rejects promise on module not found error', (t) => {
+  const f = '/fooo/bar/baz'
+
+  getLicenseInfo(f).then(() => {}).catch((err) => {
+    tap.ok(/Module not found/.test(err.message));
+
+    t.end()
+  })
+})
